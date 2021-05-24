c2ff0f8ec9c02126a9d71c115b93d5ab01f36dcd
Rakesh Singh <rakesh.s.shankala@gmail.com>
2021-05-24 14:24:55 +0530

    settings updated for pagination

--- Assignment/settings.py
+++ Assignment/settings.py
@@ -124,10 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Pagination settings
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 50
-}
